ea46f973966ab16df86eab5c2d47a5e985a26109
alinbo12 <alin.burgosolivero@gmail.com>
2022-10-25 17:00:28 -0300

    tareaArray

--- desafiosGrupales/convertir_peso.java
+++ desafiosGrupales/convertir_peso.java
@@ -12,29 +12,86 @@ muestre en pantalla el siguente mensaje según la seleccion del usuario
 
 
 import java.io.Console;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class convertir_peso {
     public static void main(String[] args){
         Console console = System.console();
 
-        int planeta = Integer.parseInt((console.readLine("Ingrese 1 si desea Marte o 2 si desea Luna:  ")));
-        switch(planeta){
-            case 1:
-            Double peso_dou = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
-            //Funcion de peso del nico sobre marte
-            Double peso_marte = (peso_dou / 9.81) * 3.711 ;
-            System.out.println("El peso en marte es : " + peso_marte);
-            break;
+        // ArrayList <Double> pesosPesado = new ArrayList <Double> (); // definimos ArrayList
 
-            case 2:
-            Double peso_dou2 = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
-            //Funcion de peso del nico sobre luna
-            Double peso_luna = (peso_dou2 / 9.81) * 1.622;
-            System.out.println("El peso en la luna es: " + peso_luna);
-            break;
+              
+       /*  int personas = Integer.parseInt(console.readLine("Ingresa cuántas personas se pesarán: ")); // ingresa la cantidad de personas
+        for(int i = 0 ; i<personas;i++){
+            int planeta = Integer.parseInt((console.readLine("Ingrese 1 si desea Marte o 2 si desea Luna:  ")));
+            switch(planeta){
+           
+                case 1:
+                Double peso_dou = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
+                //Funcion de peso del nico sobre marte
+                Double peso_marte = (peso_dou / 9.81) * 3.711 ;
+                System.out.println("El peso en marte es : " + peso_marte);
+                pesosPesado.add(peso_marte);
+                break;
+    
+                case 2:
+                Double peso_dou2 = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
+                //Funcion de peso del nico sobre luna
+                Double peso_luna = (peso_dou2 / 9.81) * 1.622;
+                System.out.println("El peso en la luna es: " + peso_luna);
+                pesosPesado.add(peso_luna);
+                break;
+    
+                default:
+                System.out.println("No seleccionaste un planeta");
 
-            default:
-            System.out.println("No seleccionaste un planeta");
-        }
         }
     }
+    System.out.println("******************************");
+    System.out.println(pesosPesado);
+   System.out.println("Haz finalizado el programa"); 
+    }
+} */
+
+Map<String, Double> nombrePeso = new HashMap <String, Double>(); 
+
+
+int personas = Integer.parseInt(console.readLine("Ingresa cuántas personas se pesarán: ")); // ingresa la cantidad de personas
+for(int i = 0 ; i<personas;i++){
+    int planeta = Integer.parseInt((console.readLine("Ingrese 1 si desea Marte o 2 si desea Luna:  ")));
+    switch(planeta){
+        case 1:
+        String nombre = console.readLine("Ingrese su nombre: ");
+        Double peso_dou = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
+        //Funcion de peso del nico sobre marte
+        Double peso_marte = (peso_dou / 9.81) * 3.711 ;
+        System.out.println("El peso en marte es : " + peso_marte);
+        nombrePeso.put(nombre, peso_marte);        
+        break;
+
+        case 2:
+         String nombre1 = console.readLine("Ingrese su nombre: ");
+        Double peso_dou2 = Double.parseDouble(console.readLine("Ingrese el peso que desea convertir: "));
+        //Funcion de peso del nico sobre luna
+        Double peso_luna = (peso_dou2 / 9.81) * 1.622;
+        System.out.println("El peso en la luna es: " + peso_luna);
+        nombrePeso.put(nombre1, peso_luna);
+        break;
+
+        default:
+        System.out.println("No seleccionaste un planeta");
+
+}
+} for (String i : nombrePeso.keySet()) {
+    System.out.println("nombres: " + i + " peso: "+ nombrePeso.get(i));
+
+  }
+  
+System.out.println("******************************");
+System.out.println("Haz finalizado el programa"); 
+
+    }
+}
+

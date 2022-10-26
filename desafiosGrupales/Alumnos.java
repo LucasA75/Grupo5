fbedc9a2c8b62ce44ea5076862238ca17c29cefb
LucasA75 <lucasacevedocastro@gmail.com>
2022-10-26 15:50:25 -0300

    contiene el if con el bucle while

--- desafiosGrupales/Alumnos.java
+++ desafiosGrupales/Alumnos.java
@@ -9,6 +9,10 @@ public class Alumnos {
         //o que me ingrese el promedio de las notas
         //Los valores se guardaran en un HashMap donde la llave sera el nombre
         //y el valor sera el arrayList de las notas
+        //el promedio tiene que salir con 1 decimal (+)
+        //ahora no se pueden ingresar notas menores a 0 y mayores a 7
+        // tiene que volver a pedir la nota
+
         Console console = System.console();
         
         Map<String, ArrayList> infoAlumno = new HashMap <String, ArrayList>();
@@ -19,19 +23,32 @@ public class Alumnos {
 
         int cantidadAlumnos = Integer.parseInt(console.readLine("Ingresa la cantidad de alumnos: "));
         for(int i=0;i<cantidadAlumnos;i++){
-            ArrayList <Double> listaNotas = new ArrayList <Double>();
-            String nombre = console.readLine("Ingrese el nombre del alumno: ");
-            int cantidadDeNotas = Integer.parseInt(console.readLine("Ingresa la cantidad De Notas: "));
-                for(int x=0; x < cantidadDeNotas;x++){
-                listaNotas.add(Double.parseDouble(console.readLine("Ingresa las notas: "))); //Aqui ingresamos las notas a un arrayList
-                infoAlumno.put(nombre,listaNotas);
-                }
-            Double sum = listaNotas.stream().mapToDouble(Double::valueOf).sum();
-            System.out.println("El Promedio es: "+ sum / cantidadDeNotas);
-            }
-        infoAlumno.forEach((k,v)-> System.out.println("Nombre: " + k + " Notas: " + v));
-
-    }
             
-
+            ArrayList <Float> listaNotas = new ArrayList <Float>();
+            String nombre = console.readLine("Ingrese el nombre del alumno: ");
+            
+            int cantidadDeNotas = Integer.parseInt(console.readLine("Ingresa la cantidad De Notas: "));
+                while(cantidadDeNotas != 0){
+                    Float nota = Float.parseFloat(console.readLine("Ingresa las notas: "));//Aqui ingresamos las notas a un arrayList
+                    
+                        if(nota >= 1 && nota <= 7){
+                            listaNotas.add(nota);
+                            --cantidadDeNotas;
+                            infoAlumno.put(nombre,listaNotas);
+                        }
+                        else{
+                            System.out.println("Ingresaste una nota incorrecta , coloca dentro una del rango del 1 al 7 ");
+                            System.out.println("la nota que ingresaste fue: " +  nota);
+                        }
+                        
+                }
+            for(String y: infoAlumno.keySet()){
+            float suma = 0.0f;
+            for(Object x : infoAlumno.get(y)){
+                suma = (Float) x + suma;
+            }
+            System.out.println("El promedio de: "+ y +" fue de: "+ Math.round(suma/cantidadAlumnos));
+        }
+    }
+    }
 }

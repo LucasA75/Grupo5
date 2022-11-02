70c9820a1842e075422f5438c4d11aeb34f5f76b
Atraides8 <dunefremens8@gmail.com>
2022-11-02 15:42:42 -0300

    Actualizacion lapiz

--- desafiosGrupales/Proyecto/firstproyect/src/main/java/com/generation/firstproyect/models/Lapiz.java
+++ desafiosGrupales/Proyecto/firstproyect/src/main/java/com/generation/firstproyect/models/Lapiz.java
@@ -1,20 +1,77 @@
 package com.generation.firstproyect.models;
 
 public class Lapiz {
+    //<Caracteristicas>
     String color;
     String material;
     String punta;
     String marca;
     int largo;
 
+    //<Constructores>
+
+    //Constructor vacio.
+    public Lapiz() {
+    }
+    
+    //Constructor con todos los datos.
+    public Lapiz(String color, String material, String punta, String marca, int largo) {
+        this.color = color;
+        this.material = material;
+        this.punta = punta;
+        this.marca = marca;
+        this.largo = largo;
+    }
+    
+    
+    //<Getters and Setters>
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public String getPunta() {
+        return punta;
+    }
+    public void setPunta(String punta) {
+        this.punta = punta;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public int getLargo() {
+        return largo;
+    }
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+
+    //<Acciones lapiz>
+    
+    //Funcion que imprime "estas escribiendo"
     void escribir(){
         System.out.println("estas escribiendo");
     }
+
+    //Funcion que te salva cuando te ahogas.
     void traqueotomia(Boolean ahogado){
         if(ahogado==true){
         System.out.println("traqueotomia en proceso");
         }
     }
+
+    //Funcion que apuñala gente.
     void apuñalar(Boolean peligro){
         if(peligro==true){
             System.out.println("apuñalar en el ojo y no retirar");

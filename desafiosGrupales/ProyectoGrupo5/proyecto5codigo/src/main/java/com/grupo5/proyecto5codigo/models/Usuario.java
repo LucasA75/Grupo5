c7bee66356118785443bdf25d2c566f4993fcd86
LucasA75 <lucasacevedocastro@gmail.com>
2022-11-04 11:46:05 -0300

    modifique archivo usuario

--- desafiosGrupales/ProyectoGrupo5/proyecto5codigo/src/main/java/com/grupo5/proyecto5codigo/models/Usuario.java
+++ desafiosGrupales/ProyectoGrupo5/proyecto5codigo/src/main/java/com/grupo5/proyecto5codigo/models/Usuario.java
@@ -1,16 +1,23 @@
 package com.grupo5.proyecto5codigo.models;
 
 public class Usuario extends Usuario_general {
-
+    
+    
+    //constructores
     public Usuario() {
+        super();
     }
 
     public Usuario(String nombre, String contraseña, String correo, int edad) {
         super(nombre, contraseña, correo, edad);
     }
 
-
+    //funciones
     
+    public void Anuncios(){
+        System.out.println("Mostrar anuncios al pobre");
+    }
+    //to string
     @Override
     public String toString() {
         return super.toString() +"Usuario []";
